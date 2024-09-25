6f6141a8d8e1c4ad28a1cd3520a1d3082dd84a75
12_1951064071_Phạm Ngọc Duy <90142625+12-1951064071-Pham-Ngoc-Duy@users.noreply.github.com>
2024-09-25 17:33:57 +0700

    panigator fe

--- store/views.py
+++ store/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
+
+from carts.models import CartItem
 from .models import Product
 from category.models import Category
+from carts.views import _cart_id
+from django.core.paginator import EmptyPage, PageNotAnInteger,Paginator
 
 def store(request, category_slug_path=None):
     categories = None
@@ -8,13 +12,19 @@ def store(request, category_slug_path=None):
     if category_slug_path != None:
         categories = get_object_or_404(Category, category_slug=category_slug_path)
         products = Product.objects.filter(category=categories, product_is_availabel=True)
+        paginator = Paginator(products, 6)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(product_is_availabel=True)
+        products = Product.objects.all().filter(product_is_availabel=True).order_by('id')
+        paginator = Paginator(products, 6)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
     
     context = {
-        'products': products,
+        'products': paged_products,
         'product_count':product_count,
     }
     return render(request, 'store/store.html', context)
@@ -22,10 +32,12 @@ def store(request, category_slug_path=None):
 def product_detail(request, category_slug_path, product_slug_path):
     try:
         single_product = Product.objects.get(category__category_slug = category_slug_path, product_slug=product_slug_path)
+        in_cart = CartItem.objects.filter(cart__cart_id = _cart_id(request), product=single_product).exists()
     except Exception as e:
         raise e
     
     context = {
         'single_product': single_product,
+        'in_cart': in_cart,
     }
     return render(request, 'store/product_detail.html', context)
